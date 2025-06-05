5b8e014e0fa32bedf10c3d4bd8279382bc3f6e4d
wells99 <9.9wellington9.9@gmail.com>
2025-06-05 17:44:48 -0300

    cores aos icones

--- src/SidebarLayout.jsx
+++ src/SidebarLayout.jsx
@@ -16,19 +16,19 @@ export default function SidebarLayout() {
   ];
 
   const estatisticas = [
-    { icone: User, nome: "Clientes", valor: "4793",cor:"blue" },
-    { icone: Package, nome: "Produtos", valor: "820",cor:"yellow" },
-    { icone: TimerReset, nome: "Serviços", valor: "4",cor:"green" },
-    { icone: FileText, nome: "Ordens", valor: "475",cor:"pink" },
-    { icone: ScrollText, nome: "Garantias", valor: "45",cor:"purple" },
-    { icone: CircleArrowUp, nome: "Receita - Dia", valor: "R$ 451",cor:"green" },
-    { icone: CircleArrowDown, nome: "Despesa - Dia", valor: "R$ 352",cor:"red" },
+    { icone: User, nome: "Clientes", valor: "4793",cor:"text-blue-600" },
+    { icone: Package, nome: "Produtos", valor: "820",cor:"text-yellow-600" },
+    { icone: TimerReset, nome: "Serviços", valor: "4",cor:"text-green-600" },
+    { icone: FileText, nome: "Ordens", valor: "475",cor:"text-pink-600" },
+    { icone: ScrollText, nome: "Garantias", valor: "45",cor:"text-purple-600" },
+    { icone: CircleArrowUp, nome: "Receita - Dia", valor: "R$ 451",cor:"text-green-600" },
+    { icone: CircleArrowDown, nome: "Despesa - Dia", valor: "R$ 352",cor:"text-red-600" },
   ]
 
   const Icone = itens[1].icone;
 
   return (
-    <div className="flex  min-h-screen ">
+    <div className="flex  min-h-screen  ">
       <aside className="w-60 bg-black " id="navbar">
         <div className="flex w-full items-center justify-center">
           <img src={logo} alt="logo" className="bg-cover bg-center" />
@@ -56,11 +56,11 @@ export default function SidebarLayout() {
                 const Icone = data.icone;
                 return (
                   <div key={index} className="w-50 h-25 bg-black rounded-xl flex hover:cursor-pointer hover:bg-neutral-900">
-                    <div className="flex flex-col flex-1 px-8">
-                      <h2 className="text-white text-sm whitespace-nowrap pt-4">{data.nome}</h2>
+                    <div className="flex flex-col flex-1 px-8 ">
+                      <h2 className=" text-sm whitespace-nowrap text-white pt-4">{data.nome}</h2>
                       <p className="text-white  pt-1">F1</p>
                     </div>
-                    <Icone className="text-white mt-8 mr-4 flex-1" />
+                    <Icone className="text-white mt-8 mr-4 flex-1 " />
                   </div>
                 );
               }
@@ -84,7 +84,7 @@ export default function SidebarLayout() {
                 const Icone = data.icone;
                 return (
                   <div key={index} className="flex items-center pl-4 gap-4 mb-4 cursor-pointer rounded-md hover:bg-neutral-200">
-                    <Icone className={`w-6 h-6 font-medium text-${data.cor}-600 font-bold`}/>
+                    <Icone className={`w-6 h-6 font-medium ${data.cor} font-bold`}/>
                     <div>
                     <h2 className={`text-black text-sm font-semibold `}>{data.nome}</h2>
                       <p className={`text-black text-xs font-semibold `}>{data.valor}</p>
