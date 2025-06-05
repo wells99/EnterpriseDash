817b49590429b801aa05d0446b4f1559f43d70dd
wells99 <9.9wellington9.9@gmail.com>
2025-06-05 18:06:31 -0300

    ajuste estatisticas

--- src/SidebarLayout.jsx
+++ src/SidebarLayout.jsx
@@ -21,6 +21,7 @@ export default function SidebarLayout() {
     { icone: TimerReset, nome: "Serviços", valor: "4",cor:"text-green-600" },
     { icone: FileText, nome: "Ordens", valor: "475",cor:"text-pink-600" },
     { icone: ScrollText, nome: "Garantias", valor: "45",cor:"text-purple-600" },
+    { icone: ScrollText, nome: "Vendas", valor: "45",cor:"text-green-700" },
     { icone: CircleArrowUp, nome: "Receita - Dia", valor: "R$ 451",cor:"text-green-600" },
     { icone: CircleArrowDown, nome: "Despesa - Dia", valor: "R$ 352",cor:"text-red-600" },
   ]
@@ -73,21 +74,21 @@ export default function SidebarLayout() {
 
         <div id="center" className="flex gap-4 mt-4">
 
-          <div className="w-[800px] h-200 bg-gray-300 rounded-md">
+          <div className="w-[800px] h-138 bg-gray-300 rounded-md ">
             <h2 className="text-left pl-4 pt-4 text-[#0d0252] font-semibold">Agenda</h2>
           </div>
-          <div className="flex-1 bg-neutral-300 rounded-md">
+          <div className="flex-1 bg-neutral-300 rounded-md h-fit">
             <h2 className="text-left pl-4 pt-4 text-[#0d0252] font-semibold">Estatísticas do Sistema</h2>
-            <div className=" px-2 py-6 w-full">
+            <div className="py-6 w-full px-2 grid grid-cols-2">
 
               {estatisticas.map((data, index) => {
                 const Icone = data.icone;
                 return (
-                  <div key={index} className="flex items-center pl-4 gap-4 mb-4 cursor-pointer rounded-md hover:bg-neutral-200">
-                    <Icone className={`w-6 h-6 font-medium ${data.cor} font-bold`}/>
+                  <div key={index} className="flex flex-col  justify-center text-center items-center px-2 py-2 gap-4 mb-4 cursor-pointer rounded-md hover:bg-neutral-200">
+                    <Icone className={`w-8 h-8  ${data.cor} font-bold`}/>
                     <div>
                     <h2 className={`text-black text-sm font-semibold `}>{data.nome}</h2>
-                      <p className={`text-black text-xs font-semibold `}>{data.valor}</p>
+                    <p className={`text-black text-xs font-semibold `}>{data.valor}</p>
                     </div>
                   </div>
                 );
